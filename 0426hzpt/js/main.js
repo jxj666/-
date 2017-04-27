c9675a847f3710fe883074967fc3d0a19409f93c
jxj322991 <228322991@qq.com>
2017-04-27 18:43:37 +0800

    0426

--- 0426hzpt/js/main.js
+++ 0426hzpt/js/main.js
@@ -1,8 +1,10 @@
-var $LN=$('#look_need')
-var $TATS=$(".tech_achieve_talents_show")
-var $TATH=$('.tech_achieve_talents_hide')
-$LN.on('click',function(){
-	console.log('1')
-	$TATS.slideDown('fast');
-	$TATH.hide();
+$(document).ready(function() {
+    var $LN = $('#look_need')
+    var $TATS = $(".tech_achieve_talents_show")
+    var $TATH = $('.tech_achieve_talents_hide')
+    $LN.on('click', function() {
+        console.log('1')
+        $TATS.slideDown('fast');
+        $TATH.hide();
+    })
 })
