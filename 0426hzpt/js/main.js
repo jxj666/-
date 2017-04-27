4873d83a0cf30421a49d7d7bdb2afeacf7c79925
jxj322991 <228322991@qq.com>
2017-04-27 17:09:42 +0800

    upadte

---
+++ 0426hzpt/js/main.js
@@ -0,0 +1,8 @@
+var $LN=$('#look_need')
+var $TATS=$(".tech_achieve_talents_show")
+var $TATH=$('.tech_achieve_talents_hide')
+$LN.on('click',function(){
+	console.log('1')
+	$TATS.slideDown('fast');
+	$TATH.hide();
+})
