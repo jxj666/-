792f84a910f4463d2dc1b843985062147428a27a
靳小健 <228322991@qq.com>
2017-06-05 15:56:25 +0800

    up

--- vue2例子/shoppingCart的副本/js/cart.js
+++ vue2例子/shoppingCart的副本/js/cart.js
@@ -1,27 +1,32 @@
 // 6月5号开始学习
 
 var vm = new Vue({
-            el: '#app',
-            data: {
-                title: '靳小健',
-                productList: [],
-                totalMoney: 0
-            },
-            filters: {},
-            mounted: function() {
-                this.cartView();
-            },
-            methods: {
-                cartView: function() {
+    el: '#app',
+    data: {
+        title: '靳小健',
+        productList: [],
+        totalMoney: 0
+    },
+    filters: {
+      formatMoney:function(val){
+        return '￥'+ val ;
+      }
+    },
+    mounted: function() {
+        this.cartView();
+    },
+    methods: {
+        cartView: function() {
 
-                    this.title += '的购物车';
-                    this.$http.get('data/cartData.json', { 'id': 123 }).then(
-                        res => {
-                            this.productList = res.body.result.list;
-                            this.totalMoney = res.body.result.totalMoney;
-                        }
-                        )
-
-                    }
+            this.title += '的购物车';
+            this.$http.get('data/cartData.json', { 'id': 123 }).then(
+                res => {
+                    this.productList = res.body.result.list;
+                    this.totalMoney = res.body.result.totalMoney;
                 }
-            })
+            )
+
+        }
+    }
+})
+
